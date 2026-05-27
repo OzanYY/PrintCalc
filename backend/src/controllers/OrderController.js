07074c46770055245210e8e8bb86fc0729077caf
OzanYY <olegnvkz22088@gmail.com>
2026-05-28 00:24:30 +0700

    фикс импорт х3

--- backend/src/controllers/OrderController.js
+++ backend/src/controllers/OrderController.js
@@ -1,4 +1,4 @@
-const OrderService = require('../services/orderService');
+const OrderService = require('../services/ OsrderService');
 const TagModel = require('../models/TagModel');
 const { getUserId, requireAuth, sendError } = require('../utils/controllerHelpers');
 
